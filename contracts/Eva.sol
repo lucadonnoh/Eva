a2705b50b1f91d6d906bfed6e3036410ee0c791e
lucadonnoh <donnoh99@gmail.com>
2021-10-03 16:24:05 +0200

    added support for contract wide royalties

--- contracts/Eva.sol
+++ contracts/Eva.sol
@@ -6,20 +6,26 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./ERC2981ContractWideRoyalties.sol";
 
-contract Eva is ERC721URIStorage, AccessControl {
+contract Eva is ERC721URIStorage, ERC2981ContractWideRoyalties, AccessControl {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC2981ContractWideRoyalties, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
     constructor() ERC721("Eva", "EVA") {
         //console.logBytes4(type(ERC721URIStorage).interfaceId);
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setRoyalties(0xf8b32D30aC6Ab3030595432533D7836FD76B078d, 1000); //to hex6c, 10% royalties
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "ipfs://...";
     }
 
     modifier onlyMinter {
--- test/sample-test.js
+++ test/sample-test.js
@@ -2,12 +2,17 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Eva", function() {
+
+    let eva;
+
+    beforeEach(async function() {
+        const Eva = await ethers.getContractFactory("Eva");
+        eva = await Eva.deploy();
+        await eva.deployed();
+    });
+
     it("Should mint a new token with correct URI", async function () {
         const [owner] = await ethers.getSigners();
-
-        const Eva = await ethers.getContractFactory("Eva");
-        const eva = await Eva.deploy();
-        await eva.deployed();
 
         const ipfsURI = "ipfs://QmbXMzoDGQRvunschhuJoSEsvyySxydrLD95dFPFJDkU7Z";
 
@@ -25,10 +30,6 @@ describe("Eva", function() {
     it("Should revert if not minter", async function () {
         const [owner, user] = await ethers.getSigners();
 
-        const Eva = await ethers.getContractFactory("Eva");
-        const eva = await Eva.deploy();
-        await eva.deployed();
-
         const ipfsURI = "ipfs://QmbXMzoDGQRvunschhuJoSEsvyySxydrLD95dFPFJDkU7Z";
 
         const minterRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
@@ -38,5 +39,28 @@ describe("Eva", function() {
 
         await expect(eva.connect(user).mint(ipfsURI)).to.be.revertedWith("Caller is not a minter");
     });
+
+    it("Should return the contract URI", async function() {
+        expect(await eva.contractURI()).to.equal("ipfs://...");
+    })
+
+    it("Should get token royalties info", async function() {
+        const [owner] = await ethers.getSigners();
+
+        const ipfsURI = "ipfs://QmbXMzoDGQRvunschhuJoSEsvyySxydrLD95dFPFJDkU7Z";
+
+        const minterRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
+
+        const grantMinterTx = await eva.grantRole(minterRole, owner.address);
+        await grantMinterTx.wait();
+
+        const mintTx = await eva.mint(ipfsURI);
+        await mintTx.wait();
+
+        expect(await eva.tokenURI(0)).to.equal(ipfsURI);
+
+        const royalties = await eva.royaltyInfo(0, 300);
+        expect(royalties[1]).to.equal(30);
+    })
 
 })
